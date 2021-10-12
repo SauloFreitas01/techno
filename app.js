75a8b19c889bc5a671ce608d28d9e73e359d2906
Saulo Freitas <69553799+Saulo-Oliveira-de-Freitas@users.noreply.github.com>
2021-10-12 18:08:58 -0300

    main files created

---
+++ app.js
@@ -0,0 +1,20 @@
+const vm =  new Vue({
+    el: "#app",
+    data() {
+        produtos:[]
+    },
+    methods: {
+        fetchProdutos(){
+            fetch("./api/produtos.json")
+            .then(res => res.json())
+            .then(res => {
+                this.produtos = res
+            })
+        }
+    },
+
+    created() {
+        this.fetchProdutos()
+    },
+
+})
